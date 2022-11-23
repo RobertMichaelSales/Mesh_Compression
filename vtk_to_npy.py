1a12d2cae867715e4a86f26d7096eb7e2511012c
RobertMichaelSales <salesmichaelrobert@gmail.com>
2022-11-23 16:42:41 +0000

    Added face and edge extraction / plotting

--- vtk_to_npy.py
+++ vtk_to_npy.py
@@ -1,15 +1,29 @@
 """ Created: 22.06.2022  \\  Updated: 22.11.2022  \\   Author: Robert Sales """
 
 #==============================================================================
+# Import libraries
 
 import vtk
 import numpy as np
-from vtk.util.numpy_support import vtk_to_numpy
-
 import matplotlib.pyplot as plt
 
+from vtk.util.numpy_support import vtk_to_numpy
+
 #==============================================================================
-# Define a function to conver 
+
+# For the purposes of this code a unit cube is defined with vertcies (x,y,z):
+    
+# A (0,0,0) 
+# B (0,1,0)      
+# C (1,0,0)      
+# D (1,1,0)       
+# E (0,0,1)
+# F (0,1,1)
+# G (1,0,1)
+# H (1,1,1)
+
+#==============================================================================
+# Define a function to import data from a VTK file and output a NPY data array 
 
 # Note: The reshaping order is non-standard as the VTK order is not contiguous
 # Note: A transpose is required so index slicing corresponds to the right axes
@@ -71,103 +85,180 @@ def VTKtoNPY(vtk_filename,variable_name,resolution):
     return npy_data
 
 #==============================================================================
-# Define a function to find the external boundary of a structured mesh/grid
+# Define a function to find the face coordinates of a structured 3D mesh
 
-def FindBoundaries(volume):
+def ExtractFaceCoords(volume,plot=True):
     
-    x_end1,x_end2 = volume[0,:,:,:],volume[-1,:,:,:]
-    y_end1,y_end2 = volume[:,0,:,:],volume[:,-1,:,:]
-    z_end1,z_end2 = volume[:,:,0,:],volume[:,:,-1,:]
+    # Extract the x-end cap faces (ABFE and DCGH)
+    face_x1 = np.reshape(volume[ 0,:,:,:3],(-1,3))
+    face_x2 = np.reshape(volume[-1,:,:,:3],(-1,3))
     
-    # Create a figure with 3-dimensional axes    
-    fig = plt.figure(figsize=(28, 28))
-    ax = fig.add_subplot(projection='3d')
+    # Extract the y-end cap faces (ADHE and BCGF)
+    face_y1 = np.reshape(volume[:, 0,:,:3],(-1,3))
+    face_y2 = np.reshape(volume[:,-1,:,:3],(-1,3))
     
-    # Plot the x-end boundaries    
-    ax.scatter(xs=x_end1[...,0], ys=x_end1[...,1], zs=x_end1[...,2], c=x_end1[...,3])
-    ax.scatter(xs=x_end2[...,0], ys=x_end2[...,1], zs=x_end2[...,2], c=x_end2[...,3])
+    # Extract the z-end cap faces (ABCD and EFGH)
+    face_z1 = np.reshape(volume[:,:, 0,:3],(-1,3))
+    face_z2 = np.reshape(volume[:,:,-1,:3],(-1,3))
     
-    # Plot the y-end boundaries    
-    ax.scatter(xs=y_end1[...,0], ys=y_end1[...,1], zs=y_end1[...,2], c=y_end1[...,3])
-    ax.scatter(xs=y_end2[...,0], ys=y_end2[...,1], zs=y_end2[...,2], c=y_end2[...,3])
+    # Add flattened face points to a list 
+    face_coords = np.vstack((face_x1,face_x2,face_y1,face_y2,face_z1,face_z2))
     
-    # Plot the y-end boundaries    
-    ax.scatter(xs=z_end1[...,0], ys=z_end1[...,1], zs=z_end1[...,2], c=z_end1[...,3])
-    ax.scatter(xs=z_end2[...,0], ys=z_end2[...,1], zs=z_end2[...,2], c=z_end2[...,3])
+    # Remove all duplicate coordinate points
+    face_coords = np.unique([tuple(row) for row in face_coords],axis=0)
     
-    # Label each of the axes
-    ax.set_xlabel('x')
-    ax.set_ylabel('y')
-    ax.set_zlabel('z')
+    if plot:
+        # Create a figure with 3-dimensional axes    
+        fig = plt.figure(figsize=(28, 28))
+        ax = fig.add_subplot(projection='3d')
 
-    # Display the figure 
-    plt.show()
+        # Plot the face points        
+        ax.scatter(xs=face_coords[...,0],ys=face_coords[...,1],zs=face_coords[...,2],c="blue",s=0.5)
+            
+        # Label each of the axes
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+        ax.set_zlabel('z')
+    
+        # Display the figure 
+        plt.show()
+    
+    return face_coords
+
+#==============================================================================
+# Define a function to plot the face surface points of a structured 3D mesh
+
+def PlotFaces(volume,plot=True):
+    
+    # Extract the x-end cap faces (ABFE and DCGH)
+    face_x1 = volume[ 0,:,:,:]
+    face_x2 = volume[-1,:,:,:]
+    
+    # Extract the y-end cap faces (ADHE and BCGF)
+    face_y1 = volume[:, 0,:,:]
+    face_y2 = volume[:,-1,:,:]
+    
+    # Extract the z-end cap faces (ABCD and EFGH)
+    face_z1 = volume[:,:, 0,:]
+    face_z2 = volume[:,:,-1,:]
+    
+    if plot:
+        # Create a figure with 3-dimensional axes    
+        fig = plt.figure(figsize=(28, 28))
+        ax = fig.add_subplot(projection='3d')
+        
+        for face in [face_x1,face_x2,face_y1,face_y2,face_z1,face_z2]:
+        
+            # Plot the end boundaries    
+            ax.scatter(xs=face[...,0],ys=face[...,1],zs=face[...,2],c="blue",s=0.5)
+            
+        # Label each of the axes
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+        ax.set_zlabel('z')
+    
+        # Display the figure 
+        plt.show()
+        
     return None
 
 #==============================================================================
-# Define a function to stack the external boundary of a structured mesh/grid
+# Define a function to find the edge coordinates of a structured 3D mesh
 
-def StackBoundaries(volume):
+def ExtractEdgeCoords(volume,plot=True):
+        
+    # Extract the x-aligned edges (AD, BC, EH and FG)
+    edge_x1 = np.reshape(volume[:, 0, 0,:3],(-1,3))
+    edge_x2 = np.reshape(volume[:,-1, 0,:3],(-1,3))
+    edge_x3 = np.reshape(volume[:, 0,-1,:3],(-1,3))
+    edge_x4 = np.reshape(volume[:,-1,-1,:3],(-1,3))
+    edge_x = np.vstack((edge_x1,edge_x2,edge_x3,edge_x4))
     
-    x_end1 = np.reshape(volume[ 0,:,:,:],(-1,volume.shape[-1]))
-    x_end2 = np.reshape(volume[-1,:,:,:],(-1,volume.shape[-1]))
-    y_end1 = np.reshape(volume[:, 0,:,:],(-1,volume.shape[-1]))
-    y_end2 = np.reshape(volume[:,-1,:,:],(-1,volume.shape[-1]))
-    z_end1 = np.reshape(volume[:,:, 0,:],(-1,volume.shape[-1]))
-    z_end2 = np.reshape(volume[:,:,-1,:],(-1,volume.shape[-1]))
+    # Extract the y-aligned edges (AB, DC, EF and HG)
+    edge_y1 = np.reshape(volume[ 0,:, 0,:3],(-1,3))
+    edge_y2 = np.reshape(volume[-1,:, 0,:3],(-1,3))
+    edge_y3 = np.reshape(volume[ 0,:,-1,:3],(-1,3))
+    edge_y4 = np.reshape(volume[-1,:,-1,:3],(-1,3))
+    edge_y = np.vstack((edge_y1,edge_y2,edge_y3,edge_y4))
     
-   ####################################################################################################### YOU GOT HERE
-   ####################################################################################################### YOU GOT HERE
-   ####################################################################################################### YOU GOT HERE
-   ####################################################################################################### YOU GOT HERE
-   ####################################################################################################### YOU GOT HERE
+    # Extract the z-aligned edges (AE, DH, BF and CG)
+    edge_z1 = np.reshape(volume[ 0, 0,:,:3],(-1,3))
+    edge_z2 = np.reshape(volume[-1, 0,:,:3],(-1,3))
+    edge_z3 = np.reshape(volume[ 0,-1,:,:3],(-1,3))
+    edge_z4 = np.reshape(volume[-1,-1,:,:3],(-1,3))
+    edge_z = np.vstack((edge_z1,edge_z2,edge_z3,edge_z4))
+
+    # Add flattened face points to a list 
+    edge_coords = np.vstack((edge_x,edge_y,edge_z))
     
+    # Remove all duplicate coordinate points
+    edge_coords = np.unique([tuple(row) for row in edge_coords],axis=0)
+    
+    if plot:
+        # Create a figure with 3-dimensional axes    
+        fig = plt.figure(figsize=(28, 28))
+        ax = fig.add_subplot(projection='3d')
+        
+        # Plot the edge points
+        ax.scatter(xs=edge_coords[...,0],ys=edge_coords[...,1],zs=edge_coords[...,2],c="blue",s=0.5) 
+        
+        # Label each of the axes
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+        ax.set_zlabel('z')
+    
+        # Display the figure 
+        plt.show()
+    
+    return edge_coords
+
 #==============================================================================
-# Define a function to plot the external boundary of a structured mesh/grid
+# Define a function to plot the edges boundaries of a structured 3D mesh
 
-def PlotBoundaries(volume):
+def PlotEdges(volume,plot=True):
     
-    # %matplotlib inline
-    # %matplotlib auto    
-
-    # Create a figure with 3-dimensional axes    
-    fig = plt.figure(figsize=(12, 12))
-    ax = fig.add_subplot(projection='3d')
+    # Extract the x-aligned edges (AD, BC, EH and FG)
+    edge_x1 = volume[:, 0, 0,:]
+    edge_x2 = volume[:,-1, 0,:]
+    edge_x3 = volume[:, 0,-1,:]
+    edge_x4 = volume[:,-1,-1,:]
     
-    # Plot the x-end boundaries
-    ax.plot_surface(X=volume[ 0,:,:,0], Y=volume[ 0,:,:,1], Z=volume[ 0,:,:,2])
-    ax.plot_surface(X=volume[-1,:,:,0], Y=volume[-1,:,:,1], Z=volume[-1,:,:,2])
-    ax.scatter(xs=volume[ 0,:,:,0], ys=volume[ 0,:,:,1], zs=volume[ 0,:,:,2], c=volume[ 0,:,:,3])
-    ax.scatter(xs=volume[-1,:,:,0], ys=volume[-1,:,:,1], zs=volume[-1,:,:,2], c=volume[-1,:,:,3])    
-
-    # Plot the y-end boundaries
-    ax.plot_surface(X=volume[:, 0,:,0], Y=volume[:, 0,:,1], Z=volume[:, 0,:,2])
-    ax.plot_surface(X=volume[:,-1,:,0], Y=volume[:,-1,:,1], Z=volume[:,-1,:,2])
-    ax.scatter(xs=volume[:, 0,:,0], ys=volume[:, 0,:,1], zs=volume[:, 0,:,2], c=volume[:, 0,:,3])
-    ax.scatter(xs=volume[:,-1,:,0], ys=volume[:,-1,:,1], zs=volume[:,-1,:,2], c=volume[:,-1,:,3])
-
-    # Plot the z-end boundaries
-    ax.plot_surface(X=volume[:,:, 0,0], Y=volume[:,:, 0,1], Z=volume[:,:, 0,2])
-    ax.plot_surface(X=volume[:,:,-1,0], Y=volume[:,:,-1,1], Z=volume[:,:,-1,2])
-    ax.scatter(xs=volume[:,:, 0,0], ys=volume[:,:, 0,1], zs=volume[:,:, 0,2], c=volume[:,:, 0,3])
-    ax.scatter(xs=volume[:,:,-1,0], ys=volume[:,:,-1,1], zs=volume[:,:,-1,2], c=volume[:,:,-1,3])
-
-    # Label each of the axes
-    ax.set_xlabel('x')
-    ax.set_ylabel('y')
-    ax.set_zlabel('z')
-
-    # Display the figure 
-    plt.show()
+    # Extract the y-aligned edges (AB, DC, EF and HG)
+    edge_y1 = volume[ 0,:, 0,:]
+    edge_y2 = volume[-1,:, 0,:]
+    edge_y3 = volume[ 0,:,-1,:]
+    edge_y4 = volume[-1,:,-1,:]
+    
+    # Extract the z-aligned edges (AE, DH, BF and CG)
+    edge_z1 = volume[ 0, 0,:,:]
+    edge_z2 = volume[-1, 0,:,:]
+    edge_z3 = volume[ 0,-1,:,:]
+    edge_z4 = volume[-1,-1,:,:]
+    
+    if plot:
+        # Create a figure with 3-dimensional axes    
+        fig = plt.figure(figsize=(28, 28))
+        ax = fig.add_subplot(projection='3d')
+        
+        # Plot the x-aligned edges  
+        for edge in [edge_x1,edge_x2,edge_x3,edge_x4]:
+            ax.scatter(xs=edge[...,0],ys=edge[...,1],zs=edge[...,2],c="blue",s=0.5) 
+        
+        # Plot the y-aligned edges  
+        for edge in [edge_y1,edge_y2,edge_y3,edge_y4]:
+            ax.scatter(xs=edge[...,0],ys=edge[...,1],zs=edge[...,2],c="blue",s=0.5) 
+        
+        # Plot the z-aligned edges  
+        for edge in [edge_z1,edge_z2,edge_z3,edge_z4]:
+            ax.scatter(xs=edge[...,0],ys=edge[...,1],zs=edge[...,2],c="blue",s=0.5) 
+        
+        # Label each of the axes
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+        ax.set_zlabel('z')
+    
+        # Display the figure 
+        plt.show()
     return None
 
 #==============================================================================
-
-
-vtk_filename = "/home/rms221/Documents/Mesh_Compression/MeshComp_AuxFiles/test_vol.vts"
-resolution = (133,49,49)
-variable_name = "V"
-
-FindBoundaries(VTKtoNPY(vtk_filename, variable_name, resolution))
-
-# PlotBoundary(VTKtoNPY(vtk_filename, variable_name, resolution))
